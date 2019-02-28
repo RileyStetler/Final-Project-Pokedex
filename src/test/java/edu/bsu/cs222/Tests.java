12bc2342d88e35fa560ec76de086f3e0783a10a2
Riley Stetler <rbstetler@bsu.edu>
2019-02-28 00:49:45 -0500

    start PokeSelection class

---
+++ src/test/java/edu/bsu/cs222/Tests.java
@@ -0,0 +1,21 @@
+package edu.bsu.cs222;
+
+import edu.bsu.cs222.PokemonSelection;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tests {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testReturnPokemon() {
+        final PokemonSelection pokemon = new PokemonSelection();
+        final int input = 004;
+        final String output = pokemon.main(input);
+        Assert.assertEquals(input, output);
+    }
+}
