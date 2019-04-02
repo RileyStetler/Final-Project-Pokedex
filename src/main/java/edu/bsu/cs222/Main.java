9c6666025320b4e2b3aa4f66c626877ee9c19376
Riley Stetler <rbstetler@bsu.edu>
2019-04-02 08:41:48 -0400

    clean up Main for presentation

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -8,16 +8,34 @@ public class Main{
         System.out.println("Enter a pokemon id to search: ");
         int id = console.nextInt();
 
-        System.out.println(PokemonSelection.PokemonId(id));
-        System.out.println(PokemonSelection.PokemonName(id));
-        System.out.println(PokemonSelection.PokemonGeneration(id));
-        System.out.println(PokemonSelection.PokemonHeight(id));
-        System.out.println(PokemonSelection.PokemonWeight(id));
-        System.out.println(PokemonSelection.PokemonBaseExperience(id));
-        System.out.println(PokemonSelection.Types(id));
-        System.out.println(PokemonSelection.Abilities(id));
-        System.out.println(PokemonSelection.HiddenAbility(id));
+        System.out.println(PokemonSelection.PokemonId(id)); //1
+        System.out.println(PokemonSelection.PokemonName(id)); //1
+        System.out.println(PokemonSelection.PokemonGeneration(id)); //1
+        System.out.println();
+        System.out.println(PokemonSelection.PokemonHeight(id)); //1
+        System.out.println(PokemonSelection.PokemonWeight(id)); //1
+        System.out.println();
+        System.out.println(PokemonSelection.PokemonBaseExperience(id)); //1
+        System.out.println();
+        System.out.println(PokemonSelection.Types(id)); //1,2
+        System.out.println();
+        System.out.println(PokemonSelection.Abilities(id)); //2
+        System.out.println(PokemonSelection.HiddenAbility(id)); //2
+        System.out.println();
         //System.out.println(PokemonSelection.PokemonBaseStat(id));
+        System.out.println(PokemonSelection.PokemonEvolutions(id)); //2
+        System.out.println(PokemonSelection.PokemonEvolutionTrigger(id)); //2
+        System.out.println();
+        System.out.println(PokemonSelection.PokemonLocation(id)); //2
+        System.out.println();
+        System.out.println(PokemonSelection.PokemonBaseAttackStat(id)); //2
+        System.out.println(PokemonSelection.PokemonBaseDefenseStat(id)); //2
+        System.out.println(PokemonSelection.PokemonBaseHealthPointsStat(id)); //2
+        System.out.println(PokemonSelection.PokemonBaseSpecialAttackStat(id)); //2
+        System.out.println(PokemonSelection.PokemonBaseSpecialDefenseStat(id)); //2
+        System.out.println(PokemonSelection.PokemonBaseSpeedStat(id)); //2
+        System.out.println();
+        System.out.println(PokemonMoveData.PokemonMove(id)); //2
 
 
         console.close();
--- src/main/java/edu/bsu/cs222/PokemonMoveData.java
+++ src/main/java/edu/bsu/cs222/PokemonMoveData.java
@@ -22,17 +22,30 @@ public class PokemonMoveData {
         return namedApiResource;
     }
 */
-    // I changed this function because every move can actually found under Pokemon
-    public static String PokemonMove(int id){
+    public static String PokemonMove(int id) {
         PokeApi pokeApi = new PokeApiClient();
         StringBuilder sb = new StringBuilder();
         List<PokemonMove> pokemonMoves = pokeApi.getPokemon(id).getMoves();
-        for (int i=0; i<pokemonMoves.size(); i++) {
-            sb.append(pokemonMoves.get(i).getMove().getName()).append("\n");
+        for (PokemonMove pokemonMove : pokemonMoves) {
+            sb.append(pokemonMove.getMove().getName()).append("\n");
         }
         return sb.toString();
     }
-/*
+/* Not in this iteration
+    //uses move ID, not Pokemon ID
+    public static String PokemonMoveType(int id) {
+        PokeApi pokeApi = new PokeApiClient();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
+        List<PokemonMove> pokemonMoves = pokeApi.getPokemon(id).getMoves();
+        for (PokemonMove pokemonMove : pokemonMoves) {
+            sb.append(pokemonMove.getMove().getName()).append("\n");
+            for (PokemonMove pokemonMove1 : pokemonMoves) {
+                sb1.append(pokeApi.getMove())
+            }
+        }
+    }
+
     public static String PokemonMoveAccuracy(int id){
         PokeApi pokeApi = new PokeApiClient();
         Move pokemonMove = pokeApi.getMove(id);
