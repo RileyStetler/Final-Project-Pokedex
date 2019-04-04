c8eaa8210c5f7c82e8c08a4eb638796614630b50
Justin <chamberlain1098@gmail.com>
2019-04-04 09:11:06 -0400

    basic format for action listeners

--- src/main/java/edu/bsu/cs222/PokeDexInterface.java
+++ src/main/java/edu/bsu/cs222/PokeDexInterface.java
@@ -1,25 +1,10 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import me.sargunvohra.lib.pokekotlin.model.Pokemon;
 
 import javax.swing.*;
-import javax.swing.plaf.MenuBarUI;
 import java.awt.*;
-
 
 
 public class PokeDexInterface extends Application {
@@ -32,14 +17,19 @@ public class PokeDexInterface extends Application {
     @Override
     public void start(Stage stage) {
 
-        JTextField heightField = new JTextField(30);
+        JTextField IDInput = new JTextField();
         JLabel bmiLabel = new JLabel("Are you sure?");
         JButton computeButton = new JButton("I Choose You!");
+
+
+        computeButton.addActionListener(e -> {
+            System.out.println(PokemonSelection.PokemonId(Integer.parseInt(IDInput.getText())));
+        });
 
         //layout
         JPanel north = new JPanel(new GridLayout(30,30));
         north.add(new JLabel("Choose Your Pokemon"));
-        north.add(heightField);
+        north.add(IDInput);
 
         // overall frame
         JFrame frame = new JFrame("Pokedex");
