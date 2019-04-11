eb5017a35a4d67ffaf58ffc7864b16c5246907af
Riley Stetler <rbstetler@bsu.edu>
2019-04-11 10:00:50 -0400

    add Pokemon move types

--- src/main/java/edu/bsu/cs222/PokemonMoveData.java
+++ src/main/java/edu/bsu/cs222/PokemonMoveData.java
@@ -8,20 +8,6 @@ import java.util.List;
 
 public class PokemonMoveData {
 
-    public static NamedApiResource NamedApiResources(int id) {
-        PokeApi pokeApi = new PokeApiClient();
-        String typeName = String.valueOf(pokeApi.getPokemon(id).getTypes());
-        NamedApiResource namedApiResource = new NamedApiResource(typeName, pokeApi.getPokemon(id).getName(), pokeApi.getPokemon(id).getId());
-        return namedApiResource;
-    }
-/*
-    public static Move NamedApiResources(int id) {
-        PokeApi pokeApi = new PokeApiClient();
-        List<PokemonMove> typeName = pokeApi.getPokemon(id).getMoves();
-        NamedApiResource namedApiResource = new NamedApiResource(typeName, pokeApi.getM);
-        return namedApiResource;
-    }
-*/
     public static String PokemonMove(int id) {
         PokeApi pokeApi = new PokeApiClient();
         StringBuilder sb = new StringBuilder();
@@ -31,25 +17,55 @@ public class PokemonMoveData {
         }
         return sb.toString();
     }
-/* Not in this iteration
+
     //uses move ID, not Pokemon ID
     public static String PokemonMoveType(int id) {
         PokeApi pokeApi = new PokeApiClient();
         StringBuilder sb = new StringBuilder();
-        StringBuilder sb1 = new StringBuilder();
         List<PokemonMove> pokemonMoves = pokeApi.getPokemon(id).getMoves();
         for (PokemonMove pokemonMove : pokemonMoves) {
-            sb.append(pokemonMove.getMove().getName()).append("\n");
-            for (PokemonMove pokemonMove1 : pokemonMoves) {
-                sb1.append(pokeApi.getMove())
-            }
+            sb.append(pokeApi.getMove(pokemonMove.getMove().getId()).getType().getName()).append("\n");
         }
+        return sb.toString();
     }
 
-    public static String PokemonMoveAccuracy(int id){
+    public static String PokemonMovePP(int id) {
         PokeApi pokeApi = new PokeApiClient();
-        Move pokemonMove = pokeApi.getMove(id);
-        //Accuracy pokemonAccuracy = pokeApii.getAccuracy(id);
-        return pokemonMove.get;
-    } */
+        StringBuilder sb = new StringBuilder();
+        List<PokemonMove> pokemonMoves = pokeApi.getPokemon(id).getMoves();
+        for (PokemonMove pokemonMove : pokemonMoves) {
+            sb.append(pokeApi.getMove(pokemonMove.getMove().getId()).getPp()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static String PokemonMovePower(int id) {
+        PokeApi pokeApi = new PokeApiClient();
+        StringBuilder sb = new StringBuilder();
+        List<PokemonMove> pokemonMoves = pokeApi.getPokemon(id).getMoves();
+        for (PokemonMove pokemonMove : pokemonMoves) {
+            sb.append(pokeApi.getMove(pokemonMove.getMove().getId()).getPower()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static String PokemonMoveAccuracy(int id) {
+        PokeApi pokeApi = new PokeApiClient();
+        StringBuilder sb = new StringBuilder();
+        List<PokemonMove> pokemonMoves = pokeApi.getPokemon(id).getMoves();
+        for (PokemonMove pokemonMove : pokemonMoves) {
+            sb.append(pokeApi.getMove(pokemonMove.getMove().getId()).getAccuracy()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static String PokemonMoveDamageClass(int id) {
+        PokeApi pokeApi = new PokeApiClient();
+        StringBuilder sb = new StringBuilder();
+        List<PokemonMove> pokemonMoves = pokeApi.getPokemon(id).getMoves();
+        for (PokemonMove pokemonMove : pokemonMoves) {
+            sb.append(pokeApi.getMove(pokemonMove.getMove().getId()).getDamageClass().getName()).append("\n");
+        }
+        return sb.toString();
+    }
 }
--- src/test/java/edu/bsu/cs222/Tests.java
+++ src/test/java/edu/bsu/cs222/Tests.java
@@ -1,6 +1,5 @@
 package edu.bsu.cs222;
 
-import me.sargunvohra.lib.pokekotlin.model.Pokemon;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -154,99 +153,7 @@ public class Tests {
         Assert.assertEquals(input, output);
     }
 
-    // There is a lot here, so hold tight.
-    @Test
-    public void testPokemonMove() {
-        final String input = PokemonMoveData.PokemonMove(1);
-        final String output = ("razor-wind\n" +
-                "swords-dance\n" +
-                "cut\n" +
-                "bind\n" +
-                "vine-whip\n" +
-                "headbutt\n" +
-                "tackle\n" +
-                "body-slam\n" +
-                "take-down\n" +
-                "double-edge\n" +
-                "growl\n" +
-                "strength\n" +
-                "mega-drain\n" +
-                "leech-seed\n" +
-                "growth\n" +
-                "razor-leaf\n" +
-                "solar-beam\n" +
-                "poison-powder\n" +
-                "sleep-powder\n" +
-                "petal-dance\n" +
-                "string-shot\n" +
-                "toxic\n" +
-                "rage\n" +
-                "mimic\n" +
-                "double-team\n" +
-                "defense-curl\n" +
-                "light-screen\n" +
-                "reflect\n" +
-                "bide\n" +
-                "sludge\n" +
-                "skull-bash\n" +
-                "amnesia\n" +
-                "flash\n" +
-                "rest\n" +
-                "substitute\n" +
-                "snore\n" +
-                "curse\n" +
-                "protect\n" +
-                "sludge-bomb\n" +
-                "mud-slap\n" +
-                "giga-drain\n" +
-                "endure\n" +
-                "charm\n" +
-                "swagger\n" +
-                "fury-cutter\n" +
-                "attract\n" +
-                "sleep-talk\n" +
-                "return\n" +
-                "frustration\n" +
-                "safeguard\n" +
-                "sweet-scent\n" +
-                "synthesis\n" +
-                "hidden-power\n" +
-                "sunny-day\n" +
-                "rock-smash\n" +
-                "facade\n" +
-                "nature-power\n" +
-                "ingrain\n" +
-                "knock-off\n" +
-                "secret-power\n" +
-                "grass-whistle\n" +
-                "bullet-seed\n" +
-                "magical-leaf\n" +
-                "natural-gift\n" +
-                "worry-seed\n" +
-                "seed-bomb\n" +
-                "energy-ball\n" +
-                "leaf-storm\n" +
-                "power-whip\n" +
-                "captivate\n" +
-                "grass-knot\n" +
-                "venoshock\n" +
-                "round\n" +
-                "echoed-voice\n" +
-                "grass-pledge\n" +
-                "work-up\n" +
-                "grassy-terrain\n" +
-                "confide\n" +
-                "");
-        Assert.assertEquals(input, output);
-    }
-/*
-    @Test
-    public void testMoveLearnMethod() {
-        final String input = PokemonMoveData.PokemonMoveAccuracy(1);
-        final String output = ("level-up");
-        Assert.assertEquals(input, output);
-    }
-*/
+
     @Test
     public void testPokemonNoEvolutions() {
         final String input = PokemonSelection.PokemonEvolutions(83);
