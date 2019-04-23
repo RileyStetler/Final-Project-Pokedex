abb6320a9667e62bbb5a0bc28a7ffef114c9ed68
justchamb <42625923+justchamb@users.noreply.github.com>
2019-04-23 14:48:07 -0400

    Able to print abilities now

--- src/main/java/edu/bsu/cs222/GUI.java
+++ src/main/java/edu/bsu/cs222/GUI.java
@@ -1,64 +1,70 @@
 package edu.bsu.cs222;
 
+import java.awt.*;
+import javax.swing.*;
 
-import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+/**
+ *
+ * @author Vincent
+ */
+public class GUI extends PokemonSelection{
 
-public class GUI extends Application {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        JFrame window = new JFrame("Button Scroll Bar");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel_1 = new JPanel();
+        JPanel panel_2 = new JPanel();
+        panel_1.setBackground(Color.gray);
+        window.add(BorderLayout.NORTH, panel_1);
+        final JScrollPane scroll_1 = new JScrollPane(panel_1, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        final JScrollPane scroll_2 = new JScrollPane(panel_2, JScrollPane.VERTICAL_SCROLLBAR_NEVER,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        panel_1.add(new JButton("Button 1"));
+        panel_1.add(new JButton("Button 2"));
+        panel_1.add(new JButton("Button 3"));
+        panel_1.add(new JButton("Button 4"));
+        panel_1.add(new JButton("Button 5"));
+        panel_1.add(new JButton("Button 6"));
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+        panel_2.add(new JButton("Button 1"));
+        panel_2.add(new JButton("Button 2"));
+        panel_2.add(new JButton("Button 3"));
+        panel_2.add(new JButton("Button 4"));
+        panel_2.add(new JButton("Button 5"));
+        panel_2.add(new JButton("Button 6"));
 
-    @Override
-    public void start(Stage primaryStage) {
-        HBox main = new HBox(15);
-        main.setAlignment(Pos.CENTER);
+        JPanel pane = new JPanel(new GridBagLayout());
 
-        VBox pokeList = new VBox();
-        Text title = new Text("Pick a pokemon");
-        pokeList.getChildren().add(title);
-        pokeList.setMaxWidth(Double.MAX_VALUE);
-        pokeList.setStyle("-fx-border-style: solid;"
-                + "-fx-border-width: 1;"
-                + "-fx-border-color: black");
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridy = GridBagConstraints.RELATIVE;
+        constraints.gridx = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1.0;
+        constraints.anchor = GridBagConstraints.NORTH;
 
-
-        for(int i=0; i<10; i++){
-            Button pokemonNames = new Button();
+        JButton b1 = new JButton();
+        JButton b2 = new JButton();
+        for (int i = 0; i < 130; i++) {
+            JButton pokemonNames = new JButton();
             pokemonNames.setText(String.valueOf(i));
-            pokemonNames.setMaxWidth(Double.MAX_VALUE);
-            pokemonNames.setOnAction((ActionEvent)->{
-                System.out.println(pokemonNames.getText());
-            });
-            pokeList.getChildren().add(pokemonNames);
+            pane.add(pokemonNames);
+
         }
+        pane.add(b1, constraints);
+        constraints.weighty = 0;
+        pane.add(b2, constraints);
+        panel_1.add(pane);
 
 
-
-        VBox pokeCharacteristics = new VBox();
-        Text title2 = new Text("Pick a pokemon");
-        pokeCharacteristics.getChildren().add(title2);
-        pokeCharacteristics.setAlignment(Pos.TOP_CENTER);
-        pokeCharacteristics.setStyle("-fx-border-style: solid;"
-                + "-fx-border-width: 1;"
-                + "-fx-border-color: black");
-
-
-
-        main.getChildren().addAll(pokeList, pokeCharacteristics);
-
-        Scene scene = new Scene(main, 500, 500);
-
-        primaryStage.setTitle("PokeDex");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        GridLayout griddy = new GridLayout(0, 2);
+        window.setLayout(griddy);
+        window.add(BorderLayout.WEST, pane);
+        window.add(BorderLayout.EAST, scroll_2);
+        window.setSize(600, 600);
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
 
     }
 }
