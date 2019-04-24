52464a223ed0421a07e1fb66cb0f2acfc70f3f7e
justchamb <42625923+justchamb@users.noreply.github.com>
2019-04-24 15:21:13 -0400

    Adding action listeners to generate data when selecting a pokemon

--- src/main/java/edu/bsu/cs222/GUI.java
+++ src/main/java/edu/bsu/cs222/GUI.java
@@ -7,55 +7,109 @@ import java.awt.*;
  *
  * @author Vincent
  */
-public class GUI extends PokemonSelection{
+public class GUI extends PokemonSelection {
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         JFrame window = new JFrame("PokeDex");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel_1 = new JPanel();
-        JPanel panel_2 = new JPanel();
-        panel_1.setBackground(Color.gray);
-        window.add(BorderLayout.NORTH, panel_1);
-        final JScrollPane scroll_1 = new JScrollPane(panel_1, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+        JPanel pokemonList = new JPanel();
+        JPanel pokemonInformation = new JPanel();
+        pokemonList.setBackground(Color.gray);
+        window.add(BorderLayout.NORTH, pokemonList);
+        final JScrollPane scroll_1 = new JScrollPane(pokemonList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         //Implement this code if we want the scroll bar on the left side
         //scroll_1.applyComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         //Allows for faster/slower scrolling
         scroll_1.getVerticalScrollBar().setUnitIncrement(15);
-        final JScrollPane scroll_2 = new JScrollPane(panel_2, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+        final JScrollPane scroll_2 = new JScrollPane(pokemonInformation, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-        panel_2.add(new JButton("Button 1"));
-        panel_2.add(new JButton("Button 2"));
-        panel_2.add(new JButton("Button 3"));
-        panel_2.add(new JButton("Button 4"));
-        panel_2.add(new JButton("Button 5"));
-        panel_2.add(new JButton("Button 6"));
 
         JPanel pane = new JPanel(new GridBagLayout());
+        Font f = new Font("Serif", Font.BOLD, 16);
 
-        for (int i = 1; i < 152; i++) {
+        for (int i = 1; i < 2; i++) {
             int id = i;
-            JButton pokemonNames = new JButton();
-            pokemonNames.setText(PokemonSelection.PokemonName(id));
-            pokemonNames.setPreferredSize(new Dimension(170,30));
-            pane.add(pokemonNames);
-            pokemonNames.addActionListener(e -> {
-                System.out.println(PokemonSelection.PokemonId(id));
-                System.out.println(PokemonSelection.PokemonName(id));
-            });
             GridBagConstraints constraints = new GridBagConstraints();
             constraints.gridy = GridBagConstraints.RELATIVE;
             constraints.gridx = 0;
             constraints.fill = GridBagConstraints.HORIZONTAL;
             constraints.weightx = 1.0;
-            constraints.anchor = GridBagConstraints.NORTH;
-            pane.add(pokemonNames, constraints);
             constraints.weighty = 0;
-            pane.add(pokemonNames, constraints);
-            panel_1.add(pane);
+            constraints.anchor = GridBagConstraints.NORTH;
 
+            JButton pokemonNames = new JButton();
+            pokemonNames.setText(PokemonSelection.PokemonId(id) + "  " + PokemonSelection.PokemonName(id).toUpperCase());
+            pokemonNames.setFont(f);
+            pokemonNames.setHorizontalAlignment(SwingConstants.LEFT);
+            pokemonNames.setPreferredSize(new Dimension(200, 30));
+
+
+            pane.add(pokemonNames);
+            pokemonNames.addActionListener(e -> {
+                pokemonInformation.removeAll();
+                System.out.println(PokemonSelection.PokemonId(id));
+                System.out.println(PokemonSelection.PokemonName(id));
+
+                JButton button;
+                JTextArea info;
+
+                GridBagLayout gridbag = new GridBagLayout();
+                GridBagConstraints c = new GridBagConstraints();
+                pokemonInformation.setLayout(gridbag);
+                c.fill = GridBagConstraints.HORIZONTAL;
+
+                info = new JTextArea(PokemonSelection.PokemonName(id));
+                c.weightx = 1.0;
+                c.gridwidth = 6;
+                c.gridx = 0;
+                c.gridy = 0;
+                info.setEditable(false);
+                gridbag.setConstraints(info, c);
+                pokemonInformation.add(info);
+
+                info = new JTextArea("ID: " + PokemonSelection.PokemonId(id));
+                c.gridx = 2;
+                c.gridy = 0;
+                gridbag.setConstraints(info, c);
+                pokemonInformation.add(info);
+
+                button = new JButton("Button 3");
+                c.gridwidth = 3;
+                c.weightx = 0.0;
+                c.gridx = 2;
+                c.gridy = 2;
+                gridbag.setConstraints(button, c);
+                pokemonInformation.add(new JButton("Button 3"));
+
+                button = new JButton("Long-Named Button 4");
+                c.ipady = 40;      //make this component tall
+                c.weightx = 0.0;
+                c.gridwidth = 3;
+                c.gridx = 0;
+                c.gridy = 1;
+                gridbag.setConstraints(button, c);
+                pokemonInformation.add(button);
+
+                button = new JButton("Button 5");
+                c.ipady = 0;       //reset to default
+                c.weighty = 1.0;   //request any extra vertical space
+                c.anchor = GridBagConstraints.SOUTH; //bottom of space
+                c.insets = new Insets(10,0,0,0);  //top padding
+                c.gridx = 1;       //aligned with button 2
+                c.gridwidth = 2;   //2 columns wide
+                c.gridy = 2;       //third row
+                gridbag.setConstraints(button, c);
+                pokemonInformation.add(button);
+
+                pokemonInformation.updateUI();
+
+            });
+
+            pane.add(pokemonNames, constraints);
+            pokemonList.add(pane);
 
         }
 
@@ -67,5 +121,19 @@ public class GUI extends PokemonSelection{
         window.setVisible(true);
         window.setLocationRelativeTo(null);
         System.out.println("DONE");
+
+        /*
+        GridBagLayout grid = new GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridy = GridBagConstraints.RELATIVE;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        constraints.weightx = 0.0;
+        constraints.gridwidth = 2;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        grid.setConstraints(panel_1, constraints);
+        window.add(panel_1);
+        */
     }
 }
--- src/main/java/edu/bsu/cs222/PokemonSelection.java
+++ src/main/java/edu/bsu/cs222/PokemonSelection.java
@@ -117,20 +117,6 @@ public class PokemonSelection {
         return pokemon + ": " + statValue;
     }
 
-//A for-loop that will print the base stats automatically
-//CURRENTLY BROKEN
-/*
-    public static String PokemonBaseStat(int id) {
-        PokeApi pokeApi = new PokeApiClient();
-        List<PokemonStat> pokemon = pokeApi.getPokemon(id).getStats();
-        for(int i = 0; i < pokemon.size(); i++){
-            String pokemonName = pokeApi.getPokemon(id).getStats().get(i).getStat().getName();
-            String statValue = String.valueOf(pokeApi.getPokemon(id).getStats().get(i).getBaseStat());
-            return pokemonName + ": " + statValue;
-        }
-        return null;
-    }
-*/
     public static String PokemonBaseSpecialDefenseStat(int id) {
         PokeApi pokeApi = new PokeApiClient();
         String pokemon = pokeApi.getPokemon(id).getStats().get(1).getStat().getName();
